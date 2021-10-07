e867f216a5b1aaa1cea30f51151c81697ca07d46
Ricardo Mannrich <mannrich@hotmail.com>
2021-10-07 09:32:29 -0300

    doc: API documentation with OpenAPI

--- src/main/kotlin/dev/ricardovm/jexchange/api/ExchangeResource.kt
+++ src/main/kotlin/dev/ricardovm/jexchange/api/ExchangeResource.kt
@@ -3,20 +3,39 @@ package dev.ricardovm.jexchange.api
 import dev.ricardovm.jexchange.domain.Currency
 import dev.ricardovm.jexchange.domain.ExchangeService
 import io.smallrye.mutiny.Uni
+import org.eclipse.microprofile.openapi.annotations.Operation
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType
+import org.eclipse.microprofile.openapi.annotations.media.Content
+import org.eclipse.microprofile.openapi.annotations.media.Schema
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse
+import org.eclipse.microprofile.openapi.annotations.tags.Tag
 import java.math.BigDecimal
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.QueryParam
+import javax.ws.rs.core.MediaType
 
 @Path("exchange")
+@Tag(name = "Exchange Resource", description = "Exchange and conversion related operations")
 class ExchangeResource(
     val exchangeService: ExchangeService
 ) {
 
     @GET
     @Path("convert")
+    @Operation(summary = "Convert an amount from a currency to other (available: BRL, EUR, JPY and USD)")
+    @APIResponse(
+        responseCode = "200",
+        content = [Content(
+            mediaType = MediaType.APPLICATION_JSON,
+            schema = Schema(
+                implementation = TransactionDTO::class,
+                type = SchemaType.OBJECT
+            )
+        )]
+    )
     fun convert(
         @Size(min = 3, max = 3, message = "Currency code should have three letters")
         @QueryParam("baseCurrency") baseCurrency: String,
--- src/main/kotlin/dev/ricardovm/jexchange/api/TransactionsResource.kt
+++ src/main/kotlin/dev/ricardovm/jexchange/api/TransactionsResource.kt
@@ -2,19 +2,38 @@ package dev.ricardovm.jexchange.api
 
 import dev.ricardovm.jexchange.domain.ExchangeService
 import io.smallrye.mutiny.Uni
+import org.eclipse.microprofile.openapi.annotations.Operation
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType
+import org.eclipse.microprofile.openapi.annotations.media.Content
+import org.eclipse.microprofile.openapi.annotations.media.Schema
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse
+import org.eclipse.microprofile.openapi.annotations.tags.Tag
 import javax.validation.constraints.NotBlank
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
+import javax.ws.rs.core.MediaType
 
 @Path("transactions")
+@Tag(name = "Transactions Resource", description = "Endpoit to query transactions")
 class TransactionsResource(
     val exchangeService: ExchangeService
 ) {
 
     @GET
     @Path("{userId}")
-    fun convert(
+    @Operation(summary = "Get all transactions made by a specific user in reverse order")
+    @APIResponse(
+        responseCode = "200",
+        content = [Content(
+            mediaType = MediaType.APPLICATION_JSON,
+            schema = org.eclipse.microprofile.openapi.annotations.media.Schema(
+                implementation = TransactionDTO::class,
+                type = SchemaType.ARRAY
+            )
+        )]
+    )
+    fun getTransactions(
         @NotBlank
         @PathParam("userId") userId: String
     ): Uni<List<TransactionDTO>> {
